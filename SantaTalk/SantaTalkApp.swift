e8f107db8ab0a34628ceac016ac1be96a318ae15
Mehedi Hasan <ron.mehedi@gmail.com>
2026-07-29 23:48:29 +0600

    Initial Commit

---
+++ SantaTalk/SantaTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SantaTalkApp.swift
+//  SantaTalk
+//
+//  Created by Mehedi Hasan Ronee on 29/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SantaTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
